5bdd11d094069735f5bb88403b285a32ffe1a2e9
Аюб Дудуев <aub.duduev.1989@gmail.com>
2023-09-23 14:24:26 +0300

    init

---
+++ ArchitectureApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ArchitectureApp
+//
+//  Created by Senior Developer on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
